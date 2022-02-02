dac0a3c337acde21eedbe3a72c288f4138fb839d
gonzalo camodeca <gonzalocamodeca@hotmail.com>
2022-02-02 00:48:59 -0300

    iconmenu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,13 @@
-import CartWidget from './CartWidget';
-
-const logoMenu = require('../assets/iconMenu.png')
+import CartWidget from "./CartWidget";
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import logoMenu from "../assets/iconMenu.png"
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-3 border-bottom border-primary p-0">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            <img className="iconMenu" src={logoMenu} alt='logo'></img>
+            <img className="iconMenu" src={logoMenu} alt="logo" />
           </a>
           <button
             className="navbar-toggler"
@@ -20,8 +20,8 @@ const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <p className="nav-link active m-2" aria-current="page" href="#">
                   Home
@@ -39,11 +39,19 @@ const Navbar = () => {
               </li>
               <li className="nav-item">
                 <p className="nav-link m-2" href="#">
-                <CartWidget></CartWidget>
+                  <CartWidget></CartWidget>
                 </p>
               </li>
             </ul>
           </div>
+          <ul className="navbar-nav ms-auto">
+            <p
+              activeClassName="active"
+              className="nav-item nav-link"
+              to="/login"
+            >Login<LoginOutlinedIcon />
+            </p>
+          </ul>
         </div>
       </nav>
     </>
