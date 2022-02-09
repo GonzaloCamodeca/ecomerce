b2927bfd753bb2e7e06a13b52fa914f225e0549e
gonzalo camodeca <gonzalocamodeca@hotmail.com>
2022-02-08 21:11:56 -0300

    error

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,38 +1,5 @@
 import Item from "../Item/Item.jsx"
 const ItemList = () =>{
-    let Juegos = [
-        {"id":1,
-        "id_game":33,
-         "thumb":"https://compass-ssl.xbox.com/assets/a4/2f/a42ff3fd-cba2-4abb-a15c-4065f76cb525.jpg?n=Mortal-Kombat-11_GLP-Page-Hero-0_1083x609_02.jpg",
-         "title":'Mortal Kombat 11 Ultimate',
-         "normalPrice": 35.99,
-         "cover_size": '50px'
-        },
-        {
-        "id":2,
-        "id_game":22,
-         "thumb":"https://www.lagzero.net/wp-content/uploads/2014/06/Mortal_Kombat_X_logo.jpg",
-         "title":'Mortal Kombat X',
-         "normalPrice": 25.99,
-         "cover_size": '50px'
-        },
-        {
-        "id":3,
-        "id_game":22,
-         "thumb":"https://www.lagzero.net/wp-content/uploads/2014/06/Mortal_Kombat_X_logo.jpg",
-         "title":'Mortal Kombat X',
-         "normalPrice": 25.99,
-         "cover_size": '50px'
-        },
-        {
-        "id":4,
-        "id_game":22,
-        "thumb":"https://www.lagzero.net/wp-content/uploads/2014/06/Mortal_Kombat_X_logo.jpg",
-        "title":'Mortal Kombat X',
-        "normalPrice": 25.99,
-        "cover_size": '50px'
-        }, 
-    ]
      return(
         <>
         {Juegos.map(item =>
